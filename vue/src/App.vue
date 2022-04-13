b12b4aacdf6b6454d3d726a3bbb4af05f6289261
A M Isaac <tangorie@gmail.com>
2022-04-13 10:17:37 -0400

    add functionality that login register logout display depending on whether logged in or not

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,15 +2,24 @@
   <div id="app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" 
-      v-if="$store.state.token != ''"
-      >Logout</router-link>
+      <router-link
+        v-bind:to="{ name: 'logout' }"
+        v-if="$store.state.token != ''"
+        >Logout</router-link
+      >
 
-      <router-link v-bind:to="{ name: 'login' }" 
-      v-if="$store.state.token == ''"
-      >Login</router-link>
+      <router-link 
+        v-bind:to="{ name: 'login' }" 
+        v-if="$store.state.token == ''"
+        >Login</router-link
+      >&nbsp;|&nbsp;
+
+      <router-link 
+      v-bind:to="{ name: 'register' }"
+       v-if="$store.state.token == ''"
+      >Register</router-link>
     </div>
-    
+
     <router-view />
   </div>
 </template>
