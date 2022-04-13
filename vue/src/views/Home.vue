1a5d9750834c194a26ac6f10defbf3cc9fc4df4a
A M Isaac <tangorie@gmail.com>
2022-04-13 10:56:37 -0400

    break - debug add brewery role

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must NOT be authenticated to see this</p>
+
+    <p 
+    v-bind:to =" this.$store.state.user" 
+    v-bind:key="this.$store.state.user.role" >
+    role:{{user.role}}
+    </p>
     <brewery-list></brewery-list>
   </div>
 </template>
