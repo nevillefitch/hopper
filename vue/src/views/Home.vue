05ad7a1b48ffae65dcdda7127d708230a06ba1da
A M Isaac <tangorie@gmail.com>
2022-04-13 11:54:09 -0400

    add access to user role so coding a different home page by user role is possible

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -2,12 +2,12 @@
   <div class="home">
     <h1>Home</h1>
     <p>You must NOT be authenticated to see this</p>
-
     <p 
-    v-bind:to =" this.$store.state.user" 
-    v-bind:key="this.$store.state.user.role" >
-    role:{{user.role}}
+    v-bind:to ="$store.state.user.authorities[0]" 
+    v-bind:key="$store.state.user.authorities.id" >
+    role:{{$store.state.user.authorities[0].name}}
     </p>
+
     <brewery-list></brewery-list>
   </div>
 </template>
--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -61,6 +61,7 @@ export default {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
             this.$router.push({name: 'home'});
+            // if brewer, route to brewer homepage
           }
         })
         .catch(error => {
