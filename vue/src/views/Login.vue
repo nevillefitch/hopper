c009bbd4c0821e9c6c61852c7dee1398fd0a184d
Luigi Ladisa <luigilad@gmail.com>
2022-04-14 10:35:52 -0400

    add return statement to getBreweryIdByOwnerId to make id available to display on homepage

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -47,43 +47,44 @@ export default {
     return {
       user: {
         username: "",
-        password: ""
+        password: "",
       },
       activeBreweryId: "",
-      invalidCredentials: false
+      invalidCredentials: false,
     };
   },
   //TODO: refactor to activeBrewery
-   created() {
-    const activeBookISBN = this.$route.params.isbn;
-    this.$store.commit("SET_ACTIVE_BOOK", activeBookISBN);
+  created() {
+    let breweryId = this.getBreweryIdByOwnerId();
+    //  const breweryId = this.$route.params.id;
+    this.$store.commit("SET_ACTIVE_BOOK", breweryId);
   },
   methods: {
-    
     login() {
       authService
         .login(this.user)
-        .then(response => {
+        .then((response) => {
           if (response.status == 200) {
             this.$store.commit("SET_AUTH_TOKEN", response.data.token);
             this.$store.commit("SET_USER", response.data.user);
-            if (this.$store.state.user.authorities[0].name == "ROLE_USER"){
+            if (this.$store.state.user.authorities[0].name == "ROLE_USER") {
               alert("test alert");
-            this.$router.push({name: 'home'});
-            }
-            else if (this.$store.state.user.authorities[0].name == "ROLE_BREWER"){
+              this.$router.push({ name: "home" });
+            } else if (
+              this.$store.state.user.authorities[0].name == "ROLE_BREWER"
+            ) {
               let breweryId = this.getBreweryIdByOwnerId();
-              this.$router.push({name: 'brewery', params: {id: breweryId}});
-            }
-            else if (this.$store.state.user.authorities[0].name == "ROLE_ADMIN"){
-              this.$router.push({name: 'home'});
+              this.$router.push({ name: "brewery", params: { id: breweryId } });
+            } else if (
+              this.$store.state.user.authorities[0].name == "ROLE_ADMIN"
+            ) {
+              this.$router.push({ name: "home" });
             }
             // if brewer, route to brewer homepage
           }
           //this.$router.push({name: 'home'});
-          
         })
-        .catch(error => {
+        .catch((error) => {
           const response = error.response;
           console.log(response);
 
@@ -94,13 +95,16 @@ export default {
     },
 
     getBreweryIdByOwnerId() {
-      for(let i = 0; i<this.$store.state.breweries.length; ++i) {
-        if (this.$store.state.breweries[i].ownerId == this.$store.state.user.id) {
-          this.activeBreweryId = this.$store.state.breweries[i].breweryId;
-          break;
-        } 
+      alert(this.$store.state.user.id);
+      for (let i = 0; i < this.$store.state.breweries.length; ++i) {
+        if (
+          this.$store.state.breweries[i].ownerId == this.$store.state.user.id
+        ) {
+          return (this.activeBreweryId =
+            this.$store.state.breweries[i].breweryId);
+        }
       }
-    }
-  }
+    },
+  },
 };
 </script>
