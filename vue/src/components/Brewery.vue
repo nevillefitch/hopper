761ba0b7bdfe15be77f522b402d4620f42d7a808
A M Isaac <tangorie@gmail.com>
2022-04-15 11:09:27 -0400

    display all detail on brewery component

--- vue/src/components/Brewery.vue
+++ vue/src/components/Brewery.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="brewery">
-    <img class="breweryLogo" :src="$store.state.activeBrewery.logo" alt="brewery logo" />
+    <img
+      class="breweryLogo"
+      :src="$store.state.activeBrewery.logo"
+      alt="brewery logo"
+    />
     <h2>{{ $store.state.activeBrewery.name }}</h2>
     <p>{{ $store.state.activeBrewery.phone }}</p>
     <p>{{ $store.state.activeBrewery.email }}</p>
-    <p>{{ $store.state.activeBrewery.city }}</p>
+    <p>{{ $store.state.activeBrewery.streetAddress }}</p>
+    <p>
+      {{ $store.state.activeBrewery.city }},
+      {{ $store.state.activeBrewery.state }}
+      {{ $store.state.activeBrewery.zipcode }}
+    </p>
+    <p>{{ $store.state.activeBrewery.history }}</p>
+    <p>{{ $store.state.activeBrewery.hasFood }}</p>
+    <p>Average price: ${{ $store.state.activeBrewery.avgPrice }}</p>
   </div>
 </template>
 
@@ -23,10 +35,7 @@ export default {
 </script>
 
 <style scoped>
-
 .breweryLogo {
-    width: 15em;
-  }
-
-
+  width: 15em;
+}
 </style>
