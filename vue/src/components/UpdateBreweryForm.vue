da0db4ec2850dc034f5ff1d88d89731256f35c86
A M Isaac <tangorie@gmail.com>
2022-04-19 15:23:45 -0400

    save css before merging branch

--- vue/src/components/UpdateBreweryForm.vue
+++ vue/src/components/UpdateBreweryForm.vue
@@ -1,114 +1,158 @@
 <template>
   <form class="update-form" v-on:submit.prevent>
-      <img
-        class="breweryLogo"
-        :src="$store.state.activeBrewery.logo"
-        alt="brewery logo"/>
-    <div class = "fields">
+    <img
+      class="breweryLogo"
+      :src="$store.state.activeBrewery.logo"
+      alt="brewery logo"
+    />
+    <div class="fields">
       <label for="logo">Brewery Logo URL: </label>
-      <input name="logo" type="url" v-model="$store.state.activeBrewery.logo"/>
+      <input name="logo" type="url" v-model="$store.state.activeBrewery.logo" />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="name">Brewery Name: </label>
-      <input name="name" type="text" v-model="$store.state.activeBrewery.name"/>
+      <input
+        name="name"
+        type="text"
+        v-model="$store.state.activeBrewery.name"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="phone">Phone: </label>
-      <input name="phone" type = "tel" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-      v-model="$store.state.activeBrewery.phone"/>
+      <input
+        name="phone"
+        type="tel"
+        placeholder="123-456-7890"
+        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+        v-model="$store.state.activeBrewery.phone"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="email">Email: </label>
-      <input name="email" type="email" v-model="$store.state.activeBrewery.email"/>
+      <input
+        name="email"
+        type="email"
+        v-model="$store.state.activeBrewery.email"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="website">Website: </label>
-      <input name="website" type="url" v-model="$store.state.activeBrewery.website"/>
+      <input
+        name="website"
+        type="url"
+        v-model="$store.state.activeBrewery.website"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="address">Address: </label>
-      <input name="address" type="text" v-model="$store.state.activeBrewery.streetAddress"/>
+      <input
+        name="address"
+        type="text"
+        v-model="$store.state.activeBrewery.streetAddress"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="city">City: </label>
-      <input name="city" type="text" v-model="$store.state.activeBrewery.city"/>
+      <input
+        name="city"
+        type="text"
+        v-model="$store.state.activeBrewery.city"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="state">State: </label>
-      <input name="state" type="text" v-model="$store.state.activeBrewery.state"/>
+      <input
+        name="state"
+        type="text"
+        v-model="$store.state.activeBrewery.state"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="zipCode">Zip code: </label>
-      <input name="zipCode" type="text" v-model="$store.state.activeBrewery.zip"/>
+      <input
+        name="zipCode"
+        type="text"
+        v-model="$store.state.activeBrewery.zip"
+      />
     </div>
-    <div class = "fields">
+    <div class="fields">
       <label for="history">History: </label>
-      <textarea name="history" v-model="$store.state.activeBrewery.history"/>
+      <textarea name="history" v-model="$store.state.activeBrewery.history" />
     </div>
-    <div class = "fields">
-      <label for="hasFoodButton">Does {{$store.state.activeBrewery.name}} have food? </label>
-      <button name="hasFoodButton" v-on:click="updateHasFood();">{{
+    <div class="fields">
+      <label for="hasFoodButton"
+        >Does {{ $store.state.activeBrewery.name }} have food?
+      </label>
+      <button name="hasFoodButton" v-on:click="updateHasFood()">
+        {{
           $store.state.activeBrewery.hasFood
             ? "Make Food Unavailable"
             : "Make Food Available"
-      }}</button>
+        }}
+      </button>
     </div>
-    <div class = "fields">
+    <div class="fields">
       <tr>
         <th>Day</th>
         <th>Open</th>
         <th>Close</th>
       </tr>
       <tr
-      v-for="day in $store.state.activeBrewery.hours"
-      v-bind:key="day.dayName"
-      class="hours"
+        v-for="day in $store.state.activeBrewery.hours"
+        v-bind:key="day.dayName"
+        class="hours"
       >
-      <td>{{day.dayName}} </td>
-      <td><input type="time" v-model="day.open"/></td>
-      <td><input type="time" v-model="day.close"/></td>
+        <td>{{ day.dayName }}</td>
+        <td><input type="time" v-model="day.open" /></td>
+        <td><input type="time" v-model="day.close" /></td>
       </tr>
-      <p>To indicate closed for the day please set both close and open times to 12:00 am.</p>
+      <p>
+        To indicate closed for the day please set both close and open times to
+        12:00 am.
+      </p>
     </div>
-    
-  <div class="submit">
+
+    <div class="submit">
+      <button type="submit" v-on:click="updateBrewery()">Update</button>
+    </div>
+    <div v-if="!this.$store.state.activeBrewery.active" class="fields">
       <button
-        type="submit"
-        v-on:click="updateBrewery();">
-        Update
+        name="activeButton"
+        v-on:click="
+          updateActiveStatus();
+          updateBrewery();
+        "
+      >
+        Activate Brewery
       </button>
-    </div>
-    <div v-if="!this.$store.state.activeBrewery.active" class = "fields">
-      <button name="activeButton" v-on:click="updateActiveStatus(); updateBrewery()">
-        Activate Brewery</button>
     </div>
   </form>
 </template>
 
 <script>
-import BreweryService from "../services/BreweryService.js"
+import BreweryService from "../services/BreweryService.js";
 export default {
   name: "update-brewery-form",
   methods: {
     updateHasFood() {
-      this.$store.commit("CHANGE_BREWERY_HAS_FOOD")
+      this.$store.commit("CHANGE_BREWERY_HAS_FOOD");
     },
     updateActiveStatus() {
-      this.$store.commit("CHANGE_BREWERY_ACTIVE_STATUS")
+      this.$store.commit("CHANGE_BREWERY_ACTIVE_STATUS");
     },
-      updateBrewery() {
+    updateBrewery() {
       BreweryService.updateBrewery(this.$store.state.activeBrewery)
-      .then((response) => {
-        if (response.status === 200) {
-          this.$router.push({name: "brewerHome"})
-          alert("success");
-        }
-      })
-      .catch((error) => {
-        this.handleErrorResponse(error, "updating");
-      });
+        .then((response) => {
+          if (response.status === 200) {
+            this.$router.push({ name: "brewerHome" });
+            alert("success");
+          }
+        })
+        .catch((error) => {
+          this.handleErrorResponse(error, "updating");
+        });
     },
-     handleErrorResponse(error, verb) {
+    handleErrorResponse(error, verb) {
       if (error.response) {
         this.errorMsg =
           "Error " +
@@ -123,23 +167,63 @@ export default {
         this.errorMsg =
           "Error " + verb + " brewery. Request could not be created.";
       }
-     },
-  }
-
-}
+    },
+  },
+};
 </script>
 
 <style>
-form.update-form{
-  width: 90%;
-  display:grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+/* Set grid areas */
+form.update-form:nth-child(1)  {
+  grid-area: logo;
+}
+form.update-form:nth-child(2) {
+  grid-area: logo-url;
+}
+form.update-form:nth-child(3) {
+  grid-area: name;
+}
+form.update-form:nth-child(4) {
+  grid-area: phone;
+}
+form.update-form:nth-child(5) {
+  grid-area: email;
+}
+form.update-form:nth-child(6) {
+  grid-area: website;
+}
+form.update-form:nth-child(7) {
+  grid-area: address;
+}
+form.update-form:nth-child(8) {
+  grid-area: city;
+}
+form.update-form:nth-child(9) {
+  grid-area: state;
+}
+form.update-form:nth-child(10) {
+  grid-area: zip;
+}
+form.update-form:nth-child(11) {
+  grid-area: history;
+}
+form.update-form:nth-child(12) {
+  grid-area: hasFood;
+}
+form.update-form:nth-child(13) {
+  grid-area: hours;
+}
+form.update-form div.submit{
+  grid-area: submit-button
+}
+form.update-form:nth-child(15) {
+  grid-area: update-button;
+}
 
-}
 .breweryLogo {
-width: 15em;
+  width: 15em;
 }
-img{
-width: 1em;
+img {
+  width: 1em;
 }
 </style>
